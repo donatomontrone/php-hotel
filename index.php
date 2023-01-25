5eb40f25b91e441c4b848151b9e4d0b65d778d6f
donatomontrone <donato.montrone@outlook.com>
2023-01-25 16:38:47 +0100

    eliminated foreach on keys and renamed the file

--- index.php
+++ index.php
@@ -60,14 +60,19 @@ Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gl
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel</title>
+    <!-- Bootstrap v5.3-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 
 <body>
     <p> <?php
         foreach ($hotels as $hotel) {
-            foreach ($hotel as $key => $value) {
-                echo "{$key}  =  {$value} <br>";
-            }
+            echo "{$hotel['name']} <br>";
+            echo "{$hotel['description']} <br>";
+            echo "{$hotel['parking']} <br>";
+            echo "{$hotel['vote']} <br>";
+            echo "{$hotel['distance_to_center']}<br>";
         }
 
         ?> </p>
